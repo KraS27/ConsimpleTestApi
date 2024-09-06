2eb178e26b81cd2cb3f7bc77e258d19ddc448492
KraSS27_Laptop <groverkras@gmail.com>
2024-09-06 13:07:55 +0300

    Config connection in Program.cs

--- ConsimpleTestApi/Program.cs
+++ ConsimpleTestApi/Program.cs
@@ -1,4 +1,8 @@
 
+using ConsimpleTestApi.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 namespace ConsimpleTestApi
 {
     public class Program
@@ -6,11 +10,20 @@ namespace ConsimpleTestApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var MsSqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(MsSqlConnectionString);
+            });
 
             var app = builder.Build();
 
